a5cfbd8e5b9369a98cc79e446e53f2ec4dae54ae
Michael Weibel <michael.weibel@gmail.com>
2013-07-02 06:56:10 +0200

    Introduction of gen_server

--- src/mod_zeug.erl
+++ src/mod_zeug.erl
@@ -9,6 +9,7 @@
 -vsn('0.1').
 
 -behaviour(gen_mod).
+-behaviour(gen_server).
 
 % required API includes from ejabberd
 -include("ejabberd.hrl").
@@ -17,9 +18,38 @@
 % gen_mod API
 -export([start/2, stop/1]).
 
+% module API
+-export([start_link/2]).
+
+% gen_server API
+-export([init/1,
+	handle_call/3, handle_cast/2, handle_info/2,
+	terminate/2, code_change/3
+]).
 
 start(_Host, _Opts) ->
 	ok.
 
 stop(_Host) ->
 	ok.
+
+start_link(_Host, _Opts) ->
+	ok.
+
+init([_Host, _Opts]) ->
+	ok.
+
+handle_call(stop, _From, State) ->
+	{stop, normal, State}.
+
+handle_cast(_Msg, State) ->
+	{noreply, State}.
+
+handle_info(_Info, State) ->
+	{noreply, State}.
+
+terminate(_Reason, _State) ->
+	ok.
+
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
