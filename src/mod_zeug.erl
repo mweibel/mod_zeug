89a75d19d3c26cd3b55c4aeafa5563269b4371af
Michael Weibel <michael.weibel@gmail.com>
2013-07-03 10:16:24 +0200

    Fix mod_zeug

--- src/mod_zeug.erl
+++ src/mod_zeug.erl
@@ -16,7 +16,7 @@
 -include("jlib.hrl").
 
 % gen_mod API
--export([start/2, stop/1]).
+-export([start/2, stop/1, log_packet_send/3]).
 
 % module API
 -export([start_link/2]).
@@ -28,9 +28,10 @@
 ]).
 
 start(Host, Opts) ->
+	?INFO_MSG("Starting ~p", [?MODULE]),
 	ejabberd_hooks:add(user_send_packet, Host, ?MODULE, log_packet_send, 55),
 	Proc = gen_mod:get_module_proc(Host, ?MODULE),
-
+	?DEBUG("Proc! ~p", [Proc]),
 	ChildSpec =
 			{Proc,
 				{?MODULE, start_link, [Host, Opts]},
@@ -38,37 +39,38 @@ start(Host, Opts) ->
 				50,
 				worker,
 				[?MODULE]},
-	supervisor:start_child(ejabberd_sup, ChildSpec).
+	?DEBUG("start: ~p", [supervisor:start_child(ejabberd_sup, ChildSpec)]).
 
 stop(Host) ->
+	?INFO_MSG("Stopping ~p", [?MODULE]),
 	ejabberd_hooks:delete(user_send_packet, Host,
 		?MODULE, log_packet_send, 55),
 	Proc = gen_mod:get_module_proc(Host, ?MODULE),
-	gen_server:call(Proc, stop),
+	%gen_server:call(Proc, stop),
 	supervisor:delete_child(ejabberd_sup, Proc).
 
-start_link(_Host, _Opts) ->
-	ok.
+start_link(Host, Opts) ->
+	Proc = gen_mod:get_module_proc(Host, ?MODULE),
+	gen_server:start_link({local, Proc}, ?MODULE, [Host, Opts], []).
 
 init([_Host, _Opts]) ->
-	?INFO_MSG("Starting ~p", [?MODULE]),
-
 	State = {},
 
 	{ok, State}.
 
 log_packet_send(From, To, Packet) ->
-	Proc = gen_mod:get_module_proc(From#jid.lserver, ?MODULE),
+	Proc = gen_mod:get_module_proc(From#jid.server, ?MODULE),
 	gen_server:cast(Proc, {log_packet, From, To, Packet}).
 
 handle_call(stop, _From, State) ->
 	{stop, normal, State}.
 
 handle_cast({log_packet, From, To, Packet}, State) ->
-	?DEBUG("Packet received:~nFrom: ~s~nTo: ~s~nPacket: ~p", [From, To, Packet]),
+	?DEBUG("Packet received:~nFrom: ~p~nTo: ~p~nPacket: ~p", [From, To, Packet]),
 	{noreply, State};
 
-handle_cast(_Msg, State) ->
+handle_cast(Msg, State) ->
+	?DEBUG("Unknown packet received ~p", [Msg]),
 	{noreply, State}.
 
 handle_info(_Info, State) ->
